d343de8cb6dc2920c8a07cd06b736429997c84e8
techiesms <sachinsms1990@gmail.com>
2017-10-21 11:44:44 +0530

    Geolocation
    This is the code written for ESP8266 12e development board for fetching
    the location data from Google server

---
+++ Geolocation.ino
@@ -0,0 +1,297 @@
+/*********************************************************************
+  This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/category/63_98
+
+  This example is for a 128x64 size display using I2C to communicate
+  3 pins are required to interface (2 I2C and one reset)
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada  for Adafruit Industries.
+  BSD license, check license.txt for more information
+  All text above, and the splash screen must be included in any redistribution
+*********************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+#include "ESP8266WiFi.h"
+
+char myssid[] = "SmS";         // your network SSID (name)
+char mypass[] = "sms123458955";          // your network password
+
+
+//Credentials for Google GeoLocation API...
+const char* Host = "www.googleapis.com";
+String thisPage = "/geolocation/v1/geolocate?key=";
+String key = "AIzaSyCYNXIYINPmTNIdusMjJloS4_BXSOff1_g";
+
+int status = WL_IDLE_STATUS;
+String jsonString = "{\n";
+
+double latitude    = 0.0;
+double longitude   = 0.0;
+double accuracy    = 0.0;
+int more_text = 1; // set to 1 for more debug output
+
+#define OLED_RESET LED_BUILTIN //4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000
+};
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+
+void setup()   {
+  Serial.begin(9600);
+
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(10,25);
+  display.println("techiesms");
+  display.setTextColor(WHITE); // 'inverted' text
+  display.display();
+
+  Serial.println("Start");
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  Serial.println("Setup done");
+  // We start by connecting to a WiFi network
+  Serial.print("Connecting to ");
+  Serial.println(myssid);
+  WiFi.begin(myssid, mypass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(".");
+
+
+}
+
+
+void loop() {
+
+  char bssid[6];
+  DynamicJsonBuffer jsonBuffer;
+  Serial.println("scan start");
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0)
+    Serial.println("no networks found");
+  else
+  {
+    Serial.print(n);
+    Serial.println(" networks found...");
+
+    if (more_text) {
+      // Print out the formatted json...
+      Serial.println("{");
+      Serial.println("\"homeMobileCountryCode\": 234,");  // this is a real UK MCC
+      Serial.println("\"homeMobileNetworkCode\": 27,");   // and a real UK MNC
+      Serial.println("\"radioType\": \"gsm\",");          // for gsm
+      Serial.println("\"carrier\": \"Vodafone\",");       // associated with Vodafone
+      //Serial.println("\"cellTowers\": [");                // I'm not reporting any cell towers
+      //Serial.println("],");
+      Serial.println("\"wifiAccessPoints\": [");
+      for (int i = 0; i < n; ++i)
+      {
+        Serial.println("{");
+        Serial.print("\"macAddress\" : \"");
+        Serial.print(WiFi.BSSIDstr(i));
+        Serial.println("\",");
+        Serial.print("\"signalStrength\": ");
+        Serial.println(WiFi.RSSI(i));
+        if (i < n - 1)
+        {
+          Serial.println("},");
+        }
+        else
+        {
+          Serial.println("}");
+        }
+      }
+      Serial.println("]");
+      Serial.println("}");
+    }
+    Serial.println(" ");
+  }
+  // now build the jsonString...
+  jsonString = "{\n";
+  jsonString += "\"homeMobileCountryCode\": 234,\n"; // this is a real UK MCC
+  jsonString += "\"homeMobileNetworkCode\": 27,\n";  // and a real UK MNC
+  jsonString += "\"radioType\": \"gsm\",\n";         // for gsm
+  jsonString += "\"carrier\": \"Vodafone\",\n";      // associated with Vodafone
+  jsonString += "\"wifiAccessPoints\": [\n";
+  for (int j = 0; j < n; ++j)
+  {
+    jsonString += "{\n";
+    jsonString += "\"macAddress\" : \"";
+    jsonString += (WiFi.BSSIDstr(j));
+    jsonString += "\",\n";
+    jsonString += "\"signalStrength\": ";
+    jsonString += WiFi.RSSI(j);
+    jsonString += "\n";
+    if (j < n - 1)
+    {
+      jsonString += "},\n";
+    }
+    else
+    {
+      jsonString += "}\n";
+    }
+  }
+  jsonString += ("]\n");
+  jsonString += ("}\n");
+  //--------------------------------------------------------------------
+
+  Serial.println("");
+
+  WiFiClientSecure client;
+
+  //Connect to the client and make the api call
+  Serial.print("Requesting URL: ");
+  Serial.println("https://" + (String)Host + thisPage + "AIzaSyCYNXIYINPmTNIdusMjJloS4_BXSOff1_g");
+  Serial.println(" ");
+  if (client.connect(Host, 443)) {
+    Serial.println("Connected");
+    client.println("POST " + thisPage + key + " HTTP/1.1");
+    client.println("Host: " + (String)Host);
+    client.println("Connection: close");
+    client.println("Content-Type: application/json");
+    client.println("User-Agent: Arduino/1.0");
+    client.print("Content-Length: ");
+    client.println(jsonString.length());
+    client.println();
+    client.print(jsonString);
+    delay(500);
+  }
+
+  //Read and parse all the lines of the reply from server
+  while (client.available()) {
+    String line = client.readStringUntil('\r');
+    if (more_text) {
+      Serial.print(line);
+    }
+    JsonObject& root = jsonBuffer.parseObject(line);
+    if (root.success()) {
+      latitude    = root["location"]["lat"];
+      longitude   = root["location"]["lng"];
+      accuracy   = root["accuracy"];
+    }
+  }
+
+  Serial.println("closing connection");
+  Serial.println();
+  client.stop();
+
+  Serial.print("Latitude = ");
+  Serial.println(latitude, 6);
+  Serial.print("Longitude = ");
+  Serial.println(longitude, 6);
+  Serial.print("Accuracy = ");
+  Serial.println(accuracy);
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(15, 0);
+  display.println("Routers");
+  display.setTextColor(WHITE); // 'inverted' text
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(40, 30);
+  display.println(n);
+  display.display();
+  delay(2000);
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(15, 0);
+  display.println("Latitude");
+  display.setTextColor(WHITE); // 'inverted' text
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(15, 30);
+  display.println(latitude, 6);
+  display.display();
+  delay(2000);
+
+  display.clearDisplay();
+
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(15, 0);
+  display.println("Longitude");
+  display.setTextColor(WHITE); // 'inverted' text
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(15, 30);
+  display.println(longitude, 6);
+
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(15, 0);
+  display.println("Accuracy");
+  display.setTextColor(WHITE); // 'inverted' text
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(20, 30);
+  display.println(accuracy);
+
+
+  display.display();
+  delay(2000);
+
+
+
+}
+
